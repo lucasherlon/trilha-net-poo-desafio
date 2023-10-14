972c0b22086ec8fe195a0f0d2b668ed39013ab26
lucasherlon <lucasherlondsmc@gmail.com>
2023-10-14 18:09:45 -0300

    resoluçao desafio smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\nTestando Iphone");
+Smartphone iphone = new Iphone("947654533", "Iphone 12", "987654321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
